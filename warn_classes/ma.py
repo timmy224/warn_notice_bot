f8b6a55d0a4e4c4ab0944dc836c63d420a2573fb
Timmy <Timmy224@gmail.com>
2023-04-08 20:43:38 -0400

    add ma

---
+++ warn_classes/ma.py
@@ -0,0 +1,61 @@
+import re
+
+import pandas as pd
+from .base_warn import Warn
+
+class MAWarn(Warn):
+    url = 'https://www.mass.gov/service-details/worker-adjustment-and-retraining-act-warn-weekly-report'
+    state = "MA"
+
+    def __init__(self, date=None):
+        super().__init__(self.url, date)
+        self.tags = "#warnact #layoffs #ma #massachusetts"
+
+    def _fetch_latest_notices(self) -> dict:
+        layoffs = {}
+        month, day, year = self._compare_date.split('/')
+        
+        formatted_date = "-".join([
+            month.zfill(2), 
+            # day.zfill(2),
+            "07",
+            year[-2:]])
+        
+        download_url = f"https://www.mass.gov/doc/warn-report-for-the-week-ending-{formatted_date}/download"
+        reader = pd.ExcelFile(download_url)
+
+        sheet_names = [s for s in reader.sheet_names]
+        df = pd.DataFrame()
+        for sheet_name in sheet_names:
+            try:
+                temp_df = pd.read_excel(
+                    download_url, 
+                    header=2, 
+                    sheet_name=sheet_name
+                )
+
+                temp_df['Date Received'] = temp_df['Date Received'].dt.strftime('%-m/%-d/%Y')
+                temp_df = temp_df[temp_df['Date Received'] == self._compare_date]
+            except:
+                temp_df = None
+            
+            if temp_df is not None:
+                df = pd.concat([df, temp_df])
+        
+        if len(df) == 0:
+            return {}
+        
+        for _, row in df.iterrows():
+            company_name = row['Company Name'].strip()
+            number_str = str(row['# Affected']).split(" ")
+
+            try:
+                number_affected = int(number_str[-1])
+            except:
+                number_affected = int(number_str[0])
+
+            if company_name not in layoffs:
+                layoffs[company_name] = 0 
+            layoffs[company_name] += number_affected
+
+        return layoffs
