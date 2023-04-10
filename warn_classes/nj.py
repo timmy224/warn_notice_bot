e54930265aa89d330983ab5a7dd162db6ce02c27
Timmy <Timmy224@gmail.com>
2023-04-10 00:01:51 -0400

    add wa

--- warn_classes/nj.py
+++ warn_classes/nj.py
@@ -10,7 +10,7 @@ class NJWarn(Warn):
 
     def __init__(self, date=None):
         super().__init__(self.url, date)
-        self.tags = "#warnact #layoffs #NJ #newjersey"
+        self.tags = "#jobs #layoffs #NJ #newjersey"
 
     def _fetch_latest_notices(self) -> dict:
         layoffs = {}
