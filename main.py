3177ccb63dd66cc2566a203d9048a2597a8a2765
Timmy <Timmy224@gmail.com>
2023-04-22 10:49:43 -0400

    add try/except for errors

--- main.py
+++ main.py
@@ -20,16 +20,23 @@ def handler(event={}, context=None) -> None:
             inspect.isclass
         )
 
+    errors = []
     for i in range(len(clsmembers)):
         bot_name = clsmembers[i][0]
         print(f'Begin {bot_name} bot')
-        warn_bot = clsmembers[i][1](date)
-        layoffs = warn_bot.fetch_latest_notices()
-        if layoffs:
-            msgs = warn_bot.create_messages(layoffs)
-            warn_bot.post_to_twitter(msgs)
-        print(f'{bot_name} has completed')
-        print()
+        try:
+            warn_bot = clsmembers[i][1](date)
+            layoffs = warn_bot.fetch_latest_notices()
+            if layoffs:
+                msgs = warn_bot.create_messages(layoffs)
+                warn_bot.post_to_twitter(msgs)
+            print(f'{bot_name} has completed')
+            print()
+        except:
+            errors.append(bot_name)
+    
+    if errors:
+        print(f"Errors fetching for the following states: {', '.join(errors)}")
 
 if __name__ == "__main__":
     handler()
